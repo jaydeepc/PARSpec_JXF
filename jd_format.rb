cdb057405ad94ff6f7b21ff023028da430452e3a
Admin <Admin@INjaydeepc.local>
2013-04-16 15:47:15 +0530

    Clearing the xml before formatting.<JD>

--- jd_format.rb
+++ jd_format.rb
@@ -3,6 +3,7 @@ require "rspec/core/formatters/base_text_formatter"
 class CustomFormatter < RSpec::Core::Formatters::BaseTextFormatter
   def initialize(output)
     super(output)
+    File.open('reports/test_results.xml', 'w') {|file| file.truncate(0) }
     @passed_tests = Array.new
     @failed_tests = Array.new
   end
